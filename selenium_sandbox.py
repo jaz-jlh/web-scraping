675754085cc0d5614283d7b385328e78155f4c30
Jaz <jlh2av@virginia.edu>
2016-12-23 15:52:33 -0500

    selenium sandbox can pull author, date, and comments from regulations.gov website

--- selenium_sandbox.py
+++ selenium_sandbox.py
@@ -1,28 +1,39 @@
 #selenium_sandbox
 
 BASE_URL = "https://www.regulations.gov/document?D=NHTSA-2016-0090-"
+#add document number to complete url
+#0001 is initial request page, 0002-1086 are comments submitted before period closed
+#as of 12/23/16 13:03 EST the highest comment ID was 1125
 
 from selenium import webdriver
 import time
 
+browser = webdriver.Firefox()		# create browser instance with Firefox
 
-browser = webdriver.Firefox()
-browser.get(BASE_URL + '0015')
-time.sleep(5)
-print("done sleeping")
-
-try:
-	elements = browser.find_elements_by_tag_name('div')
-
-
-
-	# Code for identifying locations for Author, Date, and Comment Text
-	# for i in range(len(elements)):
-	# 	if "Michael" in ((elements[i]).text):
-	# 		print("found submitter name at: " + str(i))
-	# 	if "Date Posted" in ((elements[i]).text):
-	# 		print("found date at: " + str(i))
-	# 	if "fatalities" in ((elements[i]).text):
-	# 		print("found message text at: " + str(i))
-except:
-	print("could not find elements")
+for id in range(2,10):
+	instanceURL = BASE_URL
+	for i in range(4 - len(str(id))):
+		instanceURL += "0"
+	instanceURL += str(id)
+	#print(str(id) + " : " + instanceURL)	#debug print
+	browser.get(instanceURL)
+	pageReady = 0
+	while True:
+		try:
+			elements = browser.find_elements_by_tag_name('div')
+			time.sleep(0.5)
+			print("Author: " + elements[19].text)
+			print("Date: " + elements[67].text)
+			print("Comment: " + elements[28].text)
+			# Code for identifying locations for Author, Date, and Comment Text
+			# Author: 19		Date: 67		Comment: 28
+			# for i in range(len(elements)):
+			# 	if "Michael" in ((elements[i]).text):
+			# 		print("found submitter name at: " + str(i))
+			# 	if "Date Posted" in ((elements[i]).text):
+			# 		print("found date at: " + str(i))
+			# 	if "fatalities" in ((elements[i]).text):
+			# 		print("found message text at: " + str(i))
+			break;
+		except:
+			pageReady = 0
