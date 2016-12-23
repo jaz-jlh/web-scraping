e68e8101345b5cb55d70340d45f9d5d6e86b9b9f
Jaz <jlh2av@virginia.edu>
2016-12-23 15:15:20 -0500

    identification of information location on webpage for selenium approach

--- selenium_sandbox.py
+++ selenium_sandbox.py
@@ -1,4 +1,28 @@
 #selenium_sandbox
 
+BASE_URL = "https://www.regulations.gov/document?D=NHTSA-2016-0090-"
+
 from selenium import webdriver
+import time
+
+
 browser = webdriver.Firefox()
+browser.get(BASE_URL + '0015')
+time.sleep(5)
+print("done sleeping")
+
+try:
+	elements = browser.find_elements_by_tag_name('div')
+
+
+
+	# Code for identifying locations for Author, Date, and Comment Text
+	# for i in range(len(elements)):
+	# 	if "Michael" in ((elements[i]).text):
+	# 		print("found submitter name at: " + str(i))
+	# 	if "Date Posted" in ((elements[i]).text):
+	# 		print("found date at: " + str(i))
+	# 	if "fatalities" in ((elements[i]).text):
+	# 		print("found message text at: " + str(i))
+except:
+	print("could not find elements")
